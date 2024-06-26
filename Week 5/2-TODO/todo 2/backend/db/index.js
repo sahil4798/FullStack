0e9c2b4e2b7fe5c3057e4599cb0999e582cf64d6
sahil4798 <sahilverma6353@gmail.com>
2024-06-26 23:32:02 +0530

    security push

--- Week 5/2-TODO/todo 2/backend/db/index.js
+++ Week 5/2-TODO/todo 2/backend/db/index.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect(
-  "mongodb+srv://SahilNextJs:l2O8bj5Xe8d84JFg@cluster0.bs1oqqy.mongodb.net/todos-app"
-);
+mongoose.connect();
 const todoSchema = new mongoose.Schema({
   title: String,
   description: String,
